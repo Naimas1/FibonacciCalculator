f379fd774996a12fa4b1db1b903767b61784e612
Denis Riabchuk <Naimas@DESKTOP-68Q4MHG>
2024-04-15 11:33:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FibonacciCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
